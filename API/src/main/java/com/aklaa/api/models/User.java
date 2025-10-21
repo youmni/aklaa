a4c642fe97a9db41bc9490666edb9287a774a204
youmni <146707312+youmni@users.noreply.github.com>
2025-10-21 15:24:46 +0200

    Add: User model

---
+++ API/src/main/java/com/aklaa/api/models/User.java
@@ -0,0 +1,31 @@
+package com.aklaa.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
