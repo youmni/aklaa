4e7e5023fd49c4173a7cc036fdafaec1c88eff3e
youmni <146707312+youmni@users.noreply.github.com>
2025-10-21 15:25:16 +0200

    Add: User and registrationDTO

---
+++ API/src/main/java/com/aklaa/api/dtos/RegistratieDTO.java
@@ -0,0 +1,18 @@
+package com.aklaa.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegistratieDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+}
---
+++ API/src/main/java/com/aklaa/api/dtos/UserDTO.java
@@ -0,0 +1,16 @@
+package com.aklaa.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
