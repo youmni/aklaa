49c8112fcf50de1b567784c97d0882a05cfac3e2
youmni <146707312+youmni@users.noreply.github.com>
2025-10-21 18:07:12 +0200

    Add: Validation

--- API/src/main/java/com/aklaa/api/model/User.java
+++ API/src/main/java/com/aklaa/api/model/User.java
@@ -1,6 +1,9 @@
 package com.aklaa.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,15 +25,23 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "First name is required")
     private String firstName;
 
     @Column(nullable = false)
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
     @Column(nullable = false)
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters, include upper and lower case letters, a number, and a special character"
+    )
     private String password;
 
     @Column(nullable = false)
