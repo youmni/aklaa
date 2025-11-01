34d8c3c8726d029383deee8c2bf4272baeada06e
youmni <146707312+youmni@users.noreply.github.com>
2025-11-01 14:10:18 +0100

    Add: service and controller method for updating an ingredient.

--- API/src/main/java/com/aklaa/api/services/implementation/IngredientServiceImpl.java
+++ API/src/main/java/com/aklaa/api/services/implementation/IngredientServiceImpl.java
@@ -2,11 +2,15 @@ package com.aklaa.api.services.implementation;
 
 import com.aklaa.api.dao.IngredientRepository;
 import com.aklaa.api.dtos.request.IngredientRequestDTO;
+import com.aklaa.api.dtos.response.IngredientResponseDTO;
 import com.aklaa.api.mapper.IngredientMapper;
 import com.aklaa.api.model.Ingredient;
 import com.aklaa.api.model.User;
 import com.aklaa.api.services.contract.IngredientService;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class IngredientServiceImpl implements IngredientService {
@@ -20,9 +24,27 @@ public class IngredientServiceImpl implements IngredientService {
     }
 
     @Override
-    public Ingredient create(IngredientRequestDTO ingredientRequestDTO,  User user) {
+    public IngredientResponseDTO create(IngredientRequestDTO ingredientRequestDTO,  User user) {
         Ingredient ingredient = ingredientMapper.toEntity(ingredientRequestDTO);
         ingredient.setUser(user);
-        return ingredientRepository.save(ingredient);
+        ingredientRepository.save(ingredient);
+        return ingredientMapper.toResponseDTO(ingredient);
+    }
+
+    @Override
+    public IngredientResponseDTO update(IngredientRequestDTO ingredientRequestDTO, Long id, User user) {
+        Ingredient ingredient = ingredientRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Ingredient not found"));
+
+        if(!ingredient.getUser().equals(user)){
+            throw new AccessDeniedException("Access denied");
+        }
+
+        ingredient.setName(ingredientRequestDTO.getName());
+        ingredient.setDescription(ingredientRequestDTO.getDescription());
+        ingredient.setCategory(ingredientRequestDTO.getCategory());
+        ingredient.setUnit(ingredientRequestDTO.getUnit());
+        ingredientRepository.save(ingredient);
+        return ingredientMapper.toResponseDTO(ingredient);
     }
 }
