500dea5931d9d96fe6257aca011e6315c75a6ed1
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-12-09 10:08:59 +0100

    Filter by event type

--- API/src/main/java/com/aklaa/api/services/contract/SecurityEventsService.java
+++ API/src/main/java/com/aklaa/api/services/contract/SecurityEventsService.java
@@ -30,6 +30,8 @@ public interface SecurityEventsService {
      */
     List<SecurityEventDto> getEventsSince(LocalDateTime since, Pageable pageable);
 
+    List<SecurityEventDto> getEventsSince(LocalDateTime since, SecurityEventType eventType, Pageable pageable);
+
     /**
      * Registers a new security event in the system.
      * <p>
