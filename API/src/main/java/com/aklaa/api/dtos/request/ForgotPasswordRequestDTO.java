21867be6054df66e7d5212b1d08201c34b5186ca
youmni <146707312+youmni@users.noreply.github.com>
2025-12-17 23:24:20 +0100

    Feature: Implement password reset functionality with validation

--- API/src/main/java/com/aklaa/api/dtos/request/ForgotPasswordRequestDTO.java
+++ API/src/main/java/com/aklaa/api/dtos/request/ForgotPasswordRequestDTO.java
@@ -1,7 +1,5 @@
 package com.aklaa.api.dtos.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ForgotPasswordRequestDTO {
-    @NotBlank(message = "Old Password is required")
-    private String oldPassword;
-
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
-            message = "Password must be at least 8 characters, include upper and lower case letters, a number, and a special character"
-    )
-    private String newPassword;
-
-    @NotBlank(message = "Confirm Password is required")
-    private String confirmNewPassword;
+public class PasswordResetRequestDTO {
+    private String email;
 }
